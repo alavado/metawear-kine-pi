071d4bc3ea69d3cec7081610bdbba986fe7164fd
Eric Tsai <eric@mbientlab.com>
2017-11-10 01:58:55 -0800

    Integrated command line parsing with config file

--- app.js
+++ app.js
@@ -1,14 +1,59 @@
 var MetaWear = require('metawear');
 const sensorConfig = require('./lib/sensor-config.js');
-const fs = require('fs');
-const path = require('path');
-const util = require("util");
-const moment = require("moment");
-const winston = require('winston');
+var fs = require('fs');
+var path = require('path');
+var util = require("util");
+var moment = require("moment");
+var winston = require('winston');
+var ArgumentParser = require('argparse').ArgumentParser;
 
-const CSV_DIR = "csv";
-var states = [];
-var devices = {};
+var parser = new ArgumentParser({
+    version: '1.0.0',
+    addHelp:true,
+    description: 'NodeJS app to stream sensor data to MetaHub'
+});
+parser.addArgument([ '-d', '--device' ],
+    {
+        help: 'MAC address of the device to use',
+        action: 'append',
+        metavar: 'mac',
+        type: "string"
+    }
+);
+parser.addArgument([ '--sensor' ],
+    {
+        help: 'Key-value pair that sets a sensors sampling frequency',
+        action: 'append',
+        metavar: 'sensor=freq'
+    }
+);
+parser.addArgument(['--config'],
+    {
+        help: 'Path to the config file to load',
+        metavar: 'path'
+    }
+);
+
+var args = parser.parseArgs();
+var config;
+
+if (args['config'] != null) {
+    config = JSON.parse(fs.readFileSync(args['config'], 'utf8'));
+} else if (args['device'] != null && args['sensor'] != null) {
+    config = { "devices": args['device'], "sensors": {} };
+    args['sensor'].forEach(s => {
+        const parts = s.split("=");
+        config["sensors"][parts[0]] = parseFloat(parts[1]);
+    });
+} else {
+    winston.error("either '--config' or '-d' & '-s' options must be used");
+    process.exit(0);
+}
+
+const CSV_DIR = "output";
+if (!fs.existsSync(CSV_DIR)){
+    fs.mkdirSync(CSV_DIR);
+}
 
 function findDevice(mac) {
     return new Promise((resolve, reject) => {
@@ -29,17 +74,13 @@ function findDevice(mac) {
     });
 }
 
-async function parseConfigFile(configJson) {
-    let config = await new Promise((resolve, reject) => {
-        fs.readFile(configJson, 'utf8', function (err, data) {
-            if (err) reject(err);
-            resolve(JSON.parse(data));
-        });
-    });
-    for(let k of Object.keys(config)) {
-        winston.info("Connecting to device", { 'mac': k });
+var states = [];
+var devices = [];
+(async function start() {
+    for(let d of config['devices']) {
+        winston.info("Connecting to device", { 'mac': d });
         try {
-            let device = await findDevice(k);
+            let device = await findDevice(d);
             await new Promise((resolve, reject) => {
                 var timeout = setTimeout(function() {
                     reject(null);
@@ -53,55 +94,48 @@ async function parseConfigFile(configJson) {
             });
             
             MetaWear.mbl_mw_settings_set_connection_parameters(device.board, 7.5, 7.5, 0, 6000);
-            devices[k] = device;
+            devices.push(device);
         } catch (e) {
-            winston.warn("Failed to connect to and setup device", {'mac': k});
+            winston.warn("Failed to connect to and setup device", {'mac': d});
         }
     }
 
-    if (!Object.keys(devices).length) {
+    if (!devices.length) {
         console.log("Failed to connect to any devices, terminating app");
         process.exit(0);
     }
     setTimeout(() => {
         var now = moment().format("YYYY-MM-DDTHH-mm-ss.SSS");
-        Object.keys(devices).forEach(k => {
-            Object.keys(config[k]).forEach(s => {
+        devices.forEach(d => {
+            Object.keys(config['sensors']).forEach(s => {
                 if (!(s in sensorConfig)) {
                     winston.warn(util.format("'%s' is not a valid sensor name", s));
-                } else if (!sensorConfig[s].exists(devices[k].board)) {
-                    winston.warn(util.format("'%s' does not exist on this board", s), { 'mac': k });
+                } else if (!sensorConfig[s].exists(d.board)) {
+                    winston.warn(util.format("'%s' does not exist on this board", s), { 'mac': d.address });
                 } else {
-                    let stream = fs.createWriteStream(path.join(CSV_DIR, util.format("%s_%s_%s.csv", now, k.replace(/:/g, ""), s)));
+                    let stream = fs.createWriteStream(path.join(CSV_DIR, util.format("%s_%s_%s.csv", now, d.address.replace(/:/g, ""), s)));
                     let newState = {
                         'stream': stream
                     }
                     sensorConfig[s].csvHeader(stream);
-                    MetaWear.mbl_mw_datasignal_subscribe(sensorConfig[s].signal(devices[k].board), MetaWear.FnVoid_DataP.toPointer(pointer => {
+                    MetaWear.mbl_mw_datasignal_subscribe(sensorConfig[s].signal(d.board), MetaWear.FnVoid_DataP.toPointer(pointer => {
                         sensorConfig[s].writeValue(pointer.deref(), newState);
                     }));
     
                     states.push(newState);
                 }
             });
-            Object.keys(config[k]).filter(s => sensorConfig[s].exists(devices[k].board)).forEach(s => {
-                sensorConfig[s].configure(devices[k].board, config[k][s]);
-                sensorConfig[s].start(devices[k].board);
+            Object.keys(config["sensors"]).filter(s => sensorConfig[s].exists(d.board)).forEach(s => {
+                sensorConfig[s].configure(d.board, config["sensors"][s]);
+                sensorConfig[s].start(d.board);
             });
         })
         
         process.openStdin().addListener("data", data => {
-            Object.keys(devices).forEach(k => {
-                MetaWear.mbl_mw_debug_reset(devices[k].board);
-            });
+            devices.forEach(d => MetaWear.mbl_mw_debug_reset(d.board));
             states.forEach(s => s['stream'].end());
             process.exit(0)
         });
-        winston.info("Streaming data to host device. Press any key to terminate...");
+        winston.info("Streaming data to host device. Press [Enter] to terminate...");
     }, 1000);
-}
-
-if (!fs.existsSync(CSV_DIR)){
-    fs.mkdirSync(CSV_DIR);
-}
-parseConfigFile(process.argv[2]);
+})()
